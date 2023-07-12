5fab6af31ee66cdd5181ac2d4c1a6fcaa34be400
YBowen <13163652+BowenYao18@user.noreply.gitee.com>
2023-07-12 11:51:21 +0800

    support list search

--- src/cli.py
+++ src/cli.py
@@ -3,16 +3,28 @@ from rich.console import Console
 from rich.table import Table
 
 from vtools import ESXi
-import re
 
 app = typer.Typer()
 console = Console()
 
 
+def check_criteria(vim_obj: str, criteria: str, rule: str):
+    if vim_obj == "vm":
+        if criteria == "name":
+            return esxi.list_vm(lambda vm: vm.name == rule)
+        if criteria == "path":
+            return esxi.list_vm(lambda vm: vm.path == rule)
+    if vim_obj == "datastore":
+        if criteria == "name":
+            return esxi.list_datastore(lambda ds: ds.name == rule)
+        if criteria == "type":
+            return esxi.list_datastore(lambda ds: ds.type == rule)
+
+
 @app.command()
-def list_vm():
+def list_vm(criteria: str, rule: str):
     table = Table(show_header=True, header_style="bold magenta")
-    vm_list = esxi.list_vm()
+    vm_list = check_criteria("vm", criteria, rule)
     table.add_column("Vm Name", style="dim", width=12)
     table.add_column("Path", style="dim", width=40)
     table.add_column("Power State", style="dim", width=12)
@@ -22,9 +34,9 @@ def list_vm():
 
 
 @app.command()
-def list_datastore():
+def list_datastore(criteria: str, rule: str):
     table = Table(show_header=True, header_style="bold magenta")
-    ds_list = esxi.list_datastore()
+    ds_list = check_criteria("datastore", criteria, rule)
     table.add_column("Datastore Name", style="dim", width=40)
     table.add_column("Type", style="dim", width=8)
     for ds in ds_list:
@@ -51,10 +63,10 @@ def destroy_vm(criteria: str, rule: str):
 if __name__ == "__main__":
     esxi = ESXi(ip="10.161.162.8", user="root", pwd="CSEQz4d+r8jeM*lS")
     print("+++++++++++++++++++++ Connected to ESXi ++++++++++++++++++++++++++++++++")
-
-    esxi.import_ovf(name='win11_vm',
-                    datastore=esxi.get_datastore(
-                        lambda datastore: datastore.name == 'local-0'),
-                    ovf_url='http://sftp-eng.eng.vmware.com/vmstorage/qe/windows/windows11/64/111538-Windows-11-v21H2-64-Enterprise-NVMe-Tools/111538-Windows-11-v21H2-64-Enterprise-NVMe-Tools.ovf')
-
     app()
+
+    # esxi.import_ovf(name='win11_vm',
+    #                 datastore=esxi.get_datastore(
+    #                     lambda datastore: datastore.name == 'local-0'),
+    #                 ovf_url='http://sftp-eng.eng.vmware.com/vmstorage/qe/windows/windows11/64/111538-Windows-11-v21H2-64-Enterprise-NVMe-Tools/111538-Windows-11-v21H2-64-Enterprise-NVMe-Tools.ovf')
+
